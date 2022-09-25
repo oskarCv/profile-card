e80fc091bc23135cd245c3f1951775041ddcdc82
oskacr Cv <oskar.cv91@gmail.com>
2022-09-24 22:26:30 -0500

    adding style to card

--- profile-card/src/App.js
+++ profile-card/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Profile from "./components/Profile.js";
+import background from './assets/image/DarkBlackCarbonFiberMetalLines.jpg'
+import './app.scss';
 
 function App() {
+  const bgStyle = {
+    background:`url(${background})`, backgroundSize:'cover', animation:'backgroundZoomAnimate'
+  }
+  
   return (
-    <>
+  <div className="main">
+    <div className='app' style={bgStyle}>
       <Profile/>
-    </>
+    </div>
+  </div>
   );
 }
 
